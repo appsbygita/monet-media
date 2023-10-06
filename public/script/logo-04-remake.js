908fd1f736f4ed076b7e62fbee44f0daf36bcd90
Gita Ventyana <gita.ventyana@gmail.com>
2023-10-06 11:58:24 +0700

    Change mobile dpr to 1 or 2

--- public/script/logo-04-remake.js
+++ public/script/logo-04-remake.js
@@ -768,7 +768,9 @@ function I() {
         n = min(a, l) * e,
         o = n / PX,
         c = min(2, max(devicePixelRatio ?? 1, 1));
-      c = c === 1.75 ? 1 : c;
+      if (window.screen.width < 800) {
+        c = c > 1 ? 2 : c;
+      }
       C && ((cnv.width = _W = a * c), (cnv.height = _H = l * c)),
         CTR(1, 0, 0, 1, 0, 0),
         BG(t),
